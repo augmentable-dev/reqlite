1aa1a715b01fc60fbc716a405bf1a8a8694dab63
Patrick DeVivo <patrick.devivo@gmail.com>
2021-04-26 00:51:17 -0400

    clean up the test

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/augmentable-dev/vtab v0.0.0-20210425174033-da43ecfcf070
 	github.com/go-redis/redis/v8 v8.8.2
 	github.com/go-redis/redismock/v8 v8.0.6
+	github.com/go-test/deep v1.0.7
+	github.com/jmoiron/sqlx v1.3.3
 	github.com/mattn/go-sqlite3 v1.14.7
 	go.riyazali.net/sqlite v0.0.0-20210413174544-3cf76b5afbdc
 )
--- internal/hgetall/hgetall_test.go
+++ internal/hgetall/hgetall_test.go
@@ -1,23 +1,35 @@
 package hgetall_test
 
 import (
-	"database/sql"
 	"testing"
 
 	"github.com/augmentable-dev/reqlite/internal/hgetall"
 	_ "github.com/augmentable-dev/reqlite/internal/sqlite"
 	"github.com/go-redis/redismock/v8"
+	"github.com/go-test/deep"
+	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 	"go.riyazali.net/sqlite"
 )
 
-func TestHGetAll(t *testing.T) {
-	rdb, mock := redismock.NewClientMock()
-	mock.ExpectHGetAll("test-hash").SetVal(map[string]string{
-		"one": "1",
-		"two": "2",
-	})
+type kv struct {
+	Key string `db:"hkey"`
+	Val string `db:"hval"`
+}
 
+func TestHGetAllOK(t *testing.T) {
+	rdb, mock := redismock.NewClientMock()
+
+	want := []kv{
+		{Key: "one", Val: "1"},
+		{Key: "two", Val: "2"},
+		{Key: "three", Val: "3"},
+	}
+	wantMap := make(map[string]string, len(want))
+	for _, pair := range want {
+		wantMap[pair.Key] = pair.Val
+	}
+	mock.ExpectHGetAll("test-hash").SetVal(wantMap)
 	mod := hgetall.New(rdb)
 
 	sqlite.Register(func(api *sqlite.ExtensionApi) (sqlite.ErrorCode, error) {
@@ -29,73 +41,19 @@ func TestHGetAll(t *testing.T) {
 		return sqlite.SQLITE_OK, nil
 	})
 
-	db, err := sql.Open("sqlite3", ":memory:")
+	db, err := sqlx.Open("sqlite3", ":memory:")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer db.Close()
 
-	rows, err := db.Query("SELECT * FROM HGETALL('test-hash')")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer rows.Close()
-
-	// TODO this maybe could be simplified with: https://github.com/jmoiron/sqlx
-	_, contents, err := GetContents(rows)
+	rows := []kv{}
+	err = db.Select(&rows, "SELECT * FROM HGETALL('test-hash')")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(contents) != 2 {
-		t.Fail()
+	if diff := deep.Equal(rows, want); diff != nil {
+		t.Error(diff)
 	}
-
-	if contents[0][0] != "one" || contents[0][1] != "1" {
-		t.Fail()
-	}
-
-	if contents[1][0] != "two" || contents[1][1] != "2" {
-		t.Fail()
-	}
-
-	err = rows.Err()
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func GetContents(rows *sql.Rows) (int, [][]string, error) {
-	count := 0
-	columns, err := rows.Columns()
-	if err != nil {
-		return count, nil, err
-	}
-
-	pointers := make([]interface{}, len(columns))
-	container := make([]sql.NullString, len(columns))
-	var ret [][]string
-
-	for i := range pointers {
-		pointers[i] = &container[i]
-	}
-
-	for rows.Next() {
-		err = rows.Scan(pointers...)
-		if err != nil {
-			return count, nil, err
-		}
-
-		r := make([]string, len(columns))
-		for i, c := range container {
-			if c.Valid {
-				r[i] = c.String
-			} else {
-				r[i] = "NULL"
-			}
-		}
-		ret = append(ret, r)
-	}
-	return count, ret, err
-
 }
