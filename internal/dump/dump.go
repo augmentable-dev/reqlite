5ee7364a418cb12a3527f2eddf9e8f2883318fe6
vialeon <dnewberry21@amherst.edu>
2021-06-26 10:29:36 -0400

    code cleanup

--- internal/dump/dump.go
+++ internal/dump/dump.go
@@ -15,22 +15,22 @@ type dump struct {
 func (f *dump) Args() int           { return -1 }
 func (f *dump) Deterministic() bool { return false }
 func (f *dump) Apply(ctx *sqlite.Context, values ...sqlite.Value) {
-	var (
-		key    string
-		result *redis.StringCmd
-		err    error
-	)
-	if len(values) == 1 {
-		key = values[0].Text()
+	var key string
 
+	if len(values) >= 1 {
+		key = values[0].Text()
 	} else {
-		err = fmt.Errorf("must input single arg to dump")
+		ctx.ResultError(fmt.Errorf("must supply argument to redis dump command"))
+		return
+	}
+
+	result, err := f.rdb.Dump(context.TODO(), key).Result()
+	if err != nil {
 		ctx.ResultError(err)
 		return
 	}
-	result = f.rdb.Dump(context.TODO(), key)
 
-	ctx.ResultText(fmt.Sprint(result.String(), result.FullName(), result.Name()))
+	ctx.ResultText(result)
 
 }
 
