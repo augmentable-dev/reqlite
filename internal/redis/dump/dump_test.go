5e5a0203beb1df30d953e6320302837d1494c2ec
vialeon <dnewberry21@amherst.edu>
2021-06-28 13:31:47 -0400

    add testing that trips error checks

--- internal/redis/dump/dump_test.go
+++ internal/redis/dump/dump_test.go
@@ -1,6 +1,7 @@
 package dump
 
 import (
+	"fmt"
 	"testing"
 
 	_ "github.com/augmentable-dev/reqlite/internal/sqlite"
@@ -43,4 +44,54 @@ func TestDump(t *testing.T) {
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Error(err)
 	}
+}
+func TestDumpErr(t *testing.T) {
+	rdb, _ := redismock.NewClientMock()
+
+	sqlite.Register(func(api *sqlite.ExtensionApi) (sqlite.ErrorCode, error) {
+		if err := api.CreateFunction("dump", New(rdb)); err != nil {
+			return sqlite.SQLITE_ERROR, err
+		}
+		return sqlite.SQLITE_OK, nil
+	})
+	db, err := sqlx.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	row := db.QueryRow("SELECT dump()")
+	var s string
+	err = row.Scan(&s)
+	if err == nil {
+		t.Fatal(fmt.Errorf("dump returning no error without args"))
+	}
+
+}
+func TestDumpNil(t *testing.T) {
+	rdb, mock := redismock.NewClientMock()
+
+	sqlite.Register(func(api *sqlite.ExtensionApi) (sqlite.ErrorCode, error) {
+		if err := api.CreateFunction("dump", New(rdb)); err != nil {
+			return sqlite.SQLITE_ERROR, err
+		}
+		return sqlite.SQLITE_OK, nil
+	})
+	mock.ExpectDump("nokey")
+	db, err := sqlx.Open("sqlite3", ":memory:")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	row := db.QueryRow("SELECT dump('nokey')")
+	var s string
+	err = row.Scan(&s)
+	if s != "" {
+		t.Fatalf("string value returned from uninitialized key %s", s)
+	}
+	if err == nil {
+		t.Fatal("no error from uninitialized key")
+	}
+
 }
