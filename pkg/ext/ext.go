21fb5681b0a50a7c39cbec8cedce93fa7a0834ec
vialeon <dnewberry21@amherst.edu>
2021-07-08 22:00:55 -0400

    add functions to ext.go

--- pkg/ext/ext.go
+++ pkg/ext/ext.go
@@ -8,6 +8,7 @@ import (
 	"github.com/augmentable-dev/reqlite/internal/redis/bitcount"
 	"github.com/augmentable-dev/reqlite/internal/redis/bitpos"
 	"github.com/augmentable-dev/reqlite/internal/redis/client_id"
+	"github.com/augmentable-dev/reqlite/internal/redis/cluster_countkeysinslot"
 	"github.com/augmentable-dev/reqlite/internal/redis/config_get"
 	"github.com/augmentable-dev/reqlite/internal/redis/dbsize"
 	"github.com/augmentable-dev/reqlite/internal/redis/dump"
@@ -57,6 +58,10 @@ func init() {
 		}
 
 		if err := api.CreateFunction("client_id", client_id.New(rdb)); err != nil {
+			return sqlite.SQLITE_ERROR, err
+		}
+
+		if err := api.CreateFunction("cluster_countkeysinslot", cluster_countkeysinslot.New(rdb)); err != nil {
 			return sqlite.SQLITE_ERROR, err
 		}
 
